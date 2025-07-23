9e96441356926afaa35d54fc25cabbfc2321fb58
Adarsh Tripathi <70283083+adarshtiwari1998@users.noreply.github.com>
2025-07-23 12:25:26 +0000

    created seo-utils and updated route.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
-import { generateSitemap, generateRobotsTxt } from "./ssr.js";
+import { generateSitemap, generateRobotsTxt } from "./seo-utils.js";
 // import { setupCriticalSSRRoutes } from "./seo-routes.js";
 
 export async function registerRoutes(app: Express): Promise<Server> {
