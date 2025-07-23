de066a8fc103c7f4cdda1e394f38c10ec6f70a24
Adarsh Tripathi <70283083+adarshtiwari1998@users.noreply.github.com>
2025-07-23 07:39:45 +0000

    Added new page company.tsx

--- client/src/pages/company/company.tsx
+++ client/src/pages/company/company.tsx
@@ -211,7 +211,7 @@ export default function Company() {
                 </Button>
               </Link>
               <Link href="/contact">
-                <Button size="lg" variant="outline" className="border-white text-white hover:bg-white hover:text-[hsl(134,37%,27%)]">
+                <Button size="lg" variant="outline" className="border-white text-primary hover:bg-white hover:text-[hsl(134,37%,27%)]">
                   <Calendar className="w-5 h-5 mr-2" />
                   Get In Touch
                 </Button>
